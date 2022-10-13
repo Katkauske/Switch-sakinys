674188c7c45941955df99784e67e4712d6457a44
Katkauske <kkatkauske@gmail.com>
2022-10-13 10:32:50 +0300

    Pirma

---
+++ Pirma.js
@@ -0,0 +1,48 @@
+
+let menesioSkaicius = +prompt("Noredami suzinoti menesio pavadinima iveskite skaiciu:")
+alert(menesioPavadinimas(menesioSkaicius))
+function menesioPavadinimas(menesioSkaicius) {
+    let menesioPavadinimas;
+    switch (menesioSkaicius) {
+        case 1:
+            menesioPavadinimas = "Menesio pavadinimas yra: Sausis";
+            break;
+        case 2:
+            menesioPavadinimas = "Menesio pavadinimas yra: Vasaris";
+            break;
+        case 3:
+            menesioPavadinimas = "Menesio pavadinimas yra: Kovas";
+            break;
+        case 4:
+            dmenesioPavadinimas = "Menesio pavadinimas yra: Balandis";
+            break;
+        case 5:
+            menesioPavadinimas = "Menesio pavadinimas yra: Geguze";
+            break;
+        case 6:
+            menesioPavadinimas = "Menesio pavadinimas yra: Birzelis";
+            break;
+        case 7:
+            menesioPavadinimas = "Menesio pavadinimas yra: Liepa";
+            break;
+        case 8:
+            menesioPavadinimas = "Menesio pavadinimas yra: Rugpjutis";
+            break;
+        case 9:
+            menesioPavadinimas = "Menesio pavadinimas yra: Rugsejis"
+            break;
+        case 10: 
+            menesioPavadinimas = "Menesio pavadinimas yra: Spalis"
+            break;
+        case 11:
+            menesioPavadinimas = "Menesio pavadinimas yra: Lapkritis"
+            break;
+            
+        case 12:
+            menesioPavadinimas = "Menesio pavadinimas yra: Gruodis";
+            break;
+        default:
+            
+    }
+    return menesioPavadinimas;
+}
